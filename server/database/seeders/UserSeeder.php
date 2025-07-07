fb8eb4e75b9fa94095a6f4cd9540b6a0a64bba4a
Cesar Andree <cesanmaran@gmail.com>
2025-07-07 15:49:29 -0700

    Seeders bug fixes

--- server/database/seeders/UserSeeder.php
+++ server/database/seeders/UserSeeder.php
@@ -24,5 +24,13 @@ class UserSeeder extends Seeder
             'role' => 'admin',
             'password' => bcrypt('1234'),
         ]);
+
+        // Crea un usuario user con rol user y contrasena personalizada
+        User::factory()->create([
+            'name' => 'User1',
+            'email' => 'user1@test.com',
+            'role' => 'user',
+            'password' => bcrypt('1234'),
+        ]);
     }
 }
